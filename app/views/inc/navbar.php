b5a5f7a4ca14522a2485c0902a32726cdbff0cc1
Ruvin <rubinelezi0@gmail.com>
2020-04-27 15:55:06 -0700

    ok version

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,6 +1,3 @@
-<!-- If you would like to customize the button, remove or change the "class" attribute inside the <span> tag -->
-<span class="glf-button" data-glf-cuid="e92ded68-f681-47f7-b2cc-e80646bd47d0" data-glf-ruid="8338c4da-a5cc-474e-b9a7-cea9fd9debde" > See MENU & Order</span>
-<script src="https://www.fbgcdn.com/embedder/js/ewm2.js" defer async ></script>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
     <a class="navbar-brand" href="<?php echo URL ?>users/index">Cit-as</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
